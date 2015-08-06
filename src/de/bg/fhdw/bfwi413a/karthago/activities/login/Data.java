d955ae0bf79fa38a755ed766f175545abaeddef8
An-Nam Pham <an-nam.pham@bg.fhdw.de>
2015-08-06 13:45:23 +0200

    Implement SelectionActivity, add login classes

---
+++ src/de/bg/fhdw/bfwi413a/karthago/activities/login/Data.java
@@ -0,0 +1,5 @@
+package de.bg.fhdw.bfwi413a.karthago.activities.login;
+
+public class Data {
+
+}
---
+++ src/de/bg/fhdw/bfwi413a/karthago/activities/selection/Data.java
@@ -0,0 +1,7 @@
+// Data wird benötigt, um in dieser Activity benötigten Information/Daten zu verwalten.
+
+package de.bg.fhdw.bfwi413a.karthago.activities.selection;
+
+public class Data {
+
+}
