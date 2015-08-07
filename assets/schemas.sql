76dba6c9d5deed6fb7a705e10fd72c4b045d36b5
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-08-07 12:15:47 +0200

    Move assets to dedicated directory

---
+++ assets/schemas.sql
@@ -0,0 +1,19 @@
+-- events
+-- | id | name         | timestamp | user_id | cardfile_id |
+-- |  1 | wrong-answer |       123 |       1 |           1 |
+-- |  2 | right-answer |       456 |       1 |           2 |
+-- |  3 | right-answer |       789 |       2 |           1 |
+
+-- users
+-- | id | name   |
+-- |  1 | thomas |
+-- |  2 | bernd  |
+
+-- cardfiles (retrieve from XML)
+-- | id | name |
+
+-- cards
+-- | id | uuid | user_id | evaluation_timestamp | cardfile_id |
+
+-- settings
+-- | id | key | value |
