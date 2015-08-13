1aaaac9052fa7010baba912b8d1f6ac6560a4b17
Loenify <afra2006@web.de>
2015-08-13 21:07:37 +0200

    Add startActivity Function with new Parameters for QuestionID
    Add new Navigation Function startActivity for QuestionID
    Add Calls für Navigation in ApplicationLogic

--- src/de/bg/fhdw/bfwi413a/karthago/activities/selection/ApplicationLogic.java
+++ src/de/bg/fhdw/bfwi413a/karthago/activities/selection/ApplicationLogic.java
@@ -56,6 +56,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Allgemeinwissen", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 2:   // Call CardFile2
 			//Navigation.startActivityLMode(CardFileID);
@@ -64,6 +70,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Fitness", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 3:   // Call CardFile3
 			//Navigation.startActivityLMode(CardFileID);
@@ -72,6 +84,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Musik", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 4:   // Call CardFile4
 			//Navigation.startActivityLMode(CardFileID);
@@ -80,6 +98,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Architektur", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 5:   // Call CardFile5
 			//Navigation.startActivityLMode(CardFileID);
@@ -88,6 +112,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Filme", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 6:   // Call CardFile6
 			//Navigation.startActivityLMode(CardFileID);
@@ -96,6 +126,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Kunst", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 7:   // Call CardFile7
 			//Navigation.startActivityLMode(CardFileID);
@@ -104,6 +140,12 @@ public class ApplicationLogic {
 			requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Geschichte", session.getUserDetails().toString());
 			new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		case 8:   // Call CardFile8
 			//Navigation.startActivityLMode(CardFileID);
@@ -112,6 +154,12 @@ public class ApplicationLogic {
 			//requiredIDs = dbhandler.getRequiredQuestionIDs(tstamp.getTime(), "Allgemeinwissen", session.getUserDetails().toString());
 			//new SelectionToLearnmodeHandler(requiredIDs, mContext);
 			// ---- END @author Patrick ----
+			//if(answertype = "MC"){
+			//Navigation.startActivityLM1_MC(mData.getmActivity(), questionId);}
+			//if(answertype = "FT"){
+			//Navigation.startActivityLM2_FT(mData.getmActivity(), questionId);}
+			//if(answertype = "G"){
+			//Navigation.startActivityLM3_G(mData.getmActivity(), questionId);}
 			break;
 		}
 	}
