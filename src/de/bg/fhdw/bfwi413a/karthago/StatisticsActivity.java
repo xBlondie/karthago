7127cc6e545f756780ca5a23acd3b3d141ad6e34
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-08-13 08:25:07 +0200

    Add empty statistics activity

---
+++ src/de/bg/fhdw/bfwi413a/karthago/StatisticsActivity.java
@@ -0,0 +1,15 @@
+package de.bg.fhdw.bfwi413a.karthago;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StatisticsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_statistics);
+	}
+}
