623f5cd017d6f73c9da9abbb28364147f783b5bc
Loenify <afra2006@web.de>
2015-08-31 21:59:41 +0200

    Allocate MVC Content from Init to the other Classes of LM3

--- src/de/bg/fhdw/bfwi413a/karthago/activities/lm3_g/ApplicationLogic.java
+++ src/de/bg/fhdw/bfwi413a/karthago/activities/lm3_g/ApplicationLogic.java
@@ -1,16 +1,24 @@
 package de.bg.fhdw.bfwi413a.karthago.activities.lm3_g;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class ApplicationLogic {
 	
 	private Gui mGui;
 	private Data mData;
 	Context mContext;
-	ArrayList<String> mQuestionAndAnswers;
+	String questionText;
+	String textForLevel;
 	ArrayList<String> mCorrectAnswers;
+	String user;
+    String cardfile;
 	
 	public ApplicationLogic(Gui gui, Data data, Context context) {
 		mGui = gui;
@@ -20,13 +28,56 @@ public class ApplicationLogic {
 	}
 	
 	private void initDataToGui() {
-		//TODO: Init Data from XML to TextView and Button
+		//Inits Data from XML to TextView and Button
+	    textForLevel = "Level: " + mData.getDbhandler().getCurrentLevelForQuestionId(mData.getQuestionID());
+	    mGui.setLeveltext(textForLevel);
+	    questionText = mData.getQuestionText();
+	    mGui.setQuestion(questionText);
+		mCorrectAnswers = mData.getCorrectAnswers();
+		user = mData.getSession().getUserDetails();
+		cardfile = mData.getSession().getCardfileID();
 		
 	}
 
 
 	public void onButtonClicked() {
-		//TODO: Logic for Answers
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+		builder.setTitle("Lösung");
+		TextView corAns = new TextView(mContext);
+		for(int i = 0; i < mCorrectAnswers.size(); i++){
+			corAns.append(mCorrectAnswers.get(i).toString());
+		}
+		corAns.setGravity(Gravity.CENTER_HORIZONTAL);
+		builder.setView(corAns);
+		// Add the buttons
+		builder.setPositiveButton("Habs gewusst!", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	        	boolean rightORwrong = true;
+	        	String event_name = "correct";
+	        	long tstamp = new Date().getTime();
+				mData.getDbhandler().IncreaseOrDecreaseLevelAndSetNewTimestamp(rightORwrong, mData.getQuestionID(), tstamp);
+				mData.getDbhandler().insertEvent(event_name, tstamp, user, cardfile);
+				dialog.cancel();
+				mData.getmActivity().finish();
+				mData.getApplicationLogicSelection().startSingleQuestion(mContext);
+	           }
+	       });
+		builder.setNegativeButton("Falsche Antwort!", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	        	   boolean rightORwrong = false;
+	        	   String event_name = "incorrect";
+		        	long tstamp = new Date().getTime();
+					mData.getDbhandler().IncreaseOrDecreaseLevelAndSetNewTimestamp(rightORwrong, mData.getQuestionID(), tstamp);
+					mData.getDbhandler().insertEvent(event_name, tstamp, user, cardfile);
+					dialog.cancel();
+					mData.getmActivity().finish();
+					mData.getApplicationLogicSelection().startSingleQuestion(mContext);
+	           }
+	       });
+		
+		// Create the AlertDialog
+		builder.show();
 		
 	}
 
